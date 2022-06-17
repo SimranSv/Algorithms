9b7c6483f52586b6608e84a40aec3f06d1b48d99
Simran Vedpathak <80968391+SimranSv@users.noreply.github.com>
2022-06-18 02:01:19 +0530

    Update segment_Tree.cpp

--- segment_Tree.cpp
+++ segment_Tree.cpp
@@ -1,69 +1,54 @@
-// Simran Vedpathak
-// Full Comdingbazi
-
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define pb push_back
-#define vll vector<ll>
-#define FAST                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
-
-const ll mod = 1e9 + 7;
-const ll N = 1e5 + 10;
-void build(ll ind, ll low, ll high, vll &v, vll &seg)
+class SEGTREE
 {
-    if (low == high)
+public:
+    vll seg;
+    SEGTREE(ll n)
     {
-        seg[ind] = v[low];
-        return;
+        seg.resize(4 * n);
     }
-    ll mid = (low + high) / 2;
-    build(2 * ind + 1, low, mid, v, seg);
-    build(2 * ind + 2, mid + 1, high, v, seg);
-    seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
-}
-int query(int ind, int l, int r, int low, int high, vll &seg)
-{
-    if (high < l || low > r)
-        return INT_MAX; // no overlap
-    if (low >= l && high <= r)
+    void build(ll ind, ll low, ll high, vll &v)
     {
-        return seg[ind];
+        if (low == high)
+        {
+            seg[ind] = v[low];
+            return;
+        }
+        ll mid = (low + high) / 2;
+        build(2 * ind + 1, low, mid, v);
+        build(2 * ind + 2, mid + 1, high, v);
+        seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
     }
-    int mid = (low + high) / 2;
-    int left = query(2 * ind + 1, l, r, low, mid, seg);
-    int right = query(2 * ind + 2, l, r, mid + 1, high, seg);
-    return min(left, right);
-}
-void solve()
-{
-    ll n;
-    cin >> n;
-    vll v(n);
-    for (auto &i : v)
-        cin >> i;
-    vll seg(4 * n);
-
-    build(0, 0, n - 1, v, seg);
-    ll q;
-    cin>>q;
-    while(q--){
-        ll l,r;
-        cin>>l>>r;
-
-     cout<<query(0,l,r,0,n-1,seg);
+    int query(int ind, int l, int r, int low, int high)
+    {
+        if (high < l || low > r)
+            return INT_MAX; // no overlap
+        if (low >= l && high <= r)
+        {
+            return seg[ind];
+        }
+        int mid = (low + high) / 2;
+        int left = query(2 * ind + 1, l, r, low, mid);
+        int right = query(2 * ind + 2, l, r, mid + 1, high);
+        return min(left, right);
     }
-}
-// i don't know whether i will succeed or not,but i am sure  about one thing, that i will try my best
-int main()
-{
-    FAST;
-    ll n;
-    // n = 1;
-    cin >> n;
-    while (n--)
-        solve();
-}
+    void update(int i, int ind, int change, int low, int high)
+    {
+        if (low == high)
+        {
+            seg[i] = change;
+            return;
+        }
+        ll mid = (low + high) / 2;
+        if (ind <= mid)
+            update(2 * i + 1, ind, change, low, mid);
+        else
+            update(2 * i + 2, ind, change, mid + 1, high);
+        seg[i] = min(seg[i * 2 + 1], seg[2 * i + 2]);
+    }
+    void print()
+    {
+        for (auto &i : seg)
+            cout << i << " ";
+        cout << '\n';
+    }
+};
